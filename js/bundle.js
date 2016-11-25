3a53271722a75a5bafdbeaaf5778664138135215
Jose Ignacio Gonzalez Cardenas <nacho.gon.car@gmail.com>
2016-11-24 20:50:17 -0600

    Initialize empty struct variables

--- js/bundle.js
+++ js/bundle.js
@@ -1754,11 +1754,6 @@ var compoundAssign = module.exports.compoundAssign = function(identifier, operat
 }
 
 var assign = function(identifier, tuple){
-    console.log("Assignment. Assign function.");
-    console.log("Identifier");
-    console.log(identifier);
-    console.log("Tuple");
-    console.log(tuple);
 
     // Check if identifier has already been defined in symbol table
     if(!symbolTable.lookUp(identifier.value))
@@ -1770,7 +1765,6 @@ var assign = function(identifier, tuple){
     
     // Compare types
     var idType = symbolTable.getType(identifier.value);
-    console.log(idType);
     var tupleType = tuple.type;
     
     if(!isAssignable(idType.type, tupleType))
@@ -1804,6 +1798,11 @@ var isAssignable = module.exports.isAssignable = function(objectiveType , receiv
 symbolTable = require('./symbolTable.js');
 assignment = require('./assignment.js');
 
+/* 
+* Receives an identifier, wrapped in a tuple with value and type elements.
+* The identifier represents the name of a variable declared without initialization.
+* E.g. int a;   declarator -> a
+*/ 
 simpleDeclare = module.exports.simpleDeclare = function(declarator){
     if(symbolTable.lookUp(declarator.value)){
         symbolTable.free();
@@ -1813,6 +1812,12 @@ simpleDeclare = module.exports.simpleDeclare = function(declarator){
     symbolTable.insert(declarator.value);
 }
 
+/* 
+* Receives an identifier, wrapped in a tuple with value and type elements,
+* and an initializer, also wrapped.
+* The identifier represents the name of a variable initialized with 'initializer'
+* E.g. int a = 3;   declarator -> a , initializer -> 3
+*/ 
 complexDeclare = module.exports.complexDeclare = function(declarator, initializer){
     
     simpleDeclare(declarator);
@@ -1824,10 +1829,22 @@ complexDeclare = module.exports.complexDeclare = function(declarator, initialize
     symbolTable.setObject(declarator.value, initializer);
 }
 
-// TODO: Convert inside object to declarator type
+/*
+* Receives an identifier and a type. Both wrapped in a tuple with value and type elements. See parserUtils.
+* Set type to the identifier in symbol table.
+* Because of grammar structure, initialization of value with variable occurs first.
+* Therefore type should be checked.
+* In case of struct type, all members should be initialized with undefined value.
+*/
 declareType = module.exports.declareType = function(declarator, type){
     
     console.log("Declaration.Declare type. Declarator:" + declarator + ", type:" + type);
+
+    // Send to struct initialization. Type is not yet assigned.
+    if(type.type == parserUtils.typeEnum.STRUCT_TYPE){
+        var members = initializeStruct(declarator, type);
+        symbolTable.setObject(declarator.value, members);
+    }
 
     // Declarator has no object assigned
     var objectAssigned = symbolTable.getObject(declarator.value);
@@ -1839,12 +1856,42 @@ declareType = module.exports.declareType = function(declarator, type){
     
     // Check if type can be assigned
     if(!assignment.isAssignable(type.type, objectAssigned.type)){
+        console.log("Type is not assignable");
+        console.log("Intended type:");
+        console.log(type.type);
+        console.log("Variable type:");
+        console.log(objectAssigned.type);
         symbolTable.free();
         throw new TypeError('Type ' + parserUtils.getReversedTypeEnum(objectAssigned.type) + 
                            ' can not be assigned to type ' + parserUtils.getReversedTypeEnum(type.type));
     }
-        
+    
     symbolTable.setType(declarator.value, type);
+    
+}
+
+/* 
+*   Initializes a struct in the symbol table. 
+*   All members of struct have undefined value.
+*   The value property of a struct variable is a hashmap of tuples, 
+*   with value and type.
+*/
+initializeStruct = function(structDeclarator, structType){
+    var structMembersInitialized = {};
+
+    // Get struct members from original struct specification
+    var structMembersPrototype =  symbolTable.getObject(structType.value);
+    
+    for(var i = 0; i < structMembersPrototype.length; i++){
+        var structProtoMember = structMembersPrototype[i];
+
+        // TODO Consider struct case
+        // TODO Consider pointer case
+        var initializedMember = parserUtils.generateTuple(undefined, structProtoMember.type);
+        structMembersInitialized[structProtoMember.value] = initializedMember;
+    }
+
+    return parserUtils.generateTuple(structMembersInitialized, parserUtils.typeEnum.STRUCT_TYPE);
 }
 },{"./assignment.js":9,"./symbolTable.js":12}],11:[function(require,module,exports){
 var typeEnum = module.exports.typeEnum = {
--- js/parser/declaration.js
+++ js/parser/declaration.js
@@ -1,6 +1,11 @@
 symbolTable = require('./symbolTable.js');
 assignment = require('./assignment.js');
 
+/* 
+* Receives an identifier, wrapped in a tuple with value and type elements.
+* The identifier represents the name of a variable declared without initialization.
+* E.g. int a;   declarator -> a
+*/ 
 simpleDeclare = module.exports.simpleDeclare = function(declarator){
     if(symbolTable.lookUp(declarator.value)){
         symbolTable.free();
@@ -10,6 +15,12 @@ simpleDeclare = module.exports.simpleDeclare = function(declarator){
     symbolTable.insert(declarator.value);
 }
 
+/* 
+* Receives an identifier, wrapped in a tuple with value and type elements,
+* and an initializer, also wrapped.
+* The identifier represents the name of a variable initialized with 'initializer'
+* E.g. int a = 3;   declarator -> a , initializer -> 3
+*/ 
 complexDeclare = module.exports.complexDeclare = function(declarator, initializer){
     
     simpleDeclare(declarator);
@@ -21,10 +32,22 @@ complexDeclare = module.exports.complexDeclare = function(declarator, initialize
     symbolTable.setObject(declarator.value, initializer);
 }
 
-// TODO: Convert inside object to declarator type
+/*
+* Receives an identifier and a type. Both wrapped in a tuple with value and type elements. See parserUtils.
+* Set type to the identifier in symbol table.
+* Because of grammar structure, initialization of value with variable occurs first.
+* Therefore type should be checked.
+* In case of struct type, all members should be initialized with undefined value.
+*/
 declareType = module.exports.declareType = function(declarator, type){
     
     console.log("Declaration.Declare type. Declarator:" + declarator + ", type:" + type);
+
+    // Send to struct initialization. Type is not yet assigned.
+    if(type.type == parserUtils.typeEnum.STRUCT_TYPE){
+        var members = initializeStruct(declarator, type);
+        symbolTable.setObject(declarator.value, members);
+    }
 
     // Declarator has no object assigned
     var objectAssigned = symbolTable.getObject(declarator.value);
@@ -36,10 +59,40 @@ declareType = module.exports.declareType = function(declarator, type){
     
     // Check if type can be assigned
     if(!assignment.isAssignable(type.type, objectAssigned.type)){
+        console.log("Type is not assignable");
+        console.log("Intended type:");
+        console.log(type.type);
+        console.log("Variable type:");
+        console.log(objectAssigned.type);
         symbolTable.free();
         throw new TypeError('Type ' + parserUtils.getReversedTypeEnum(objectAssigned.type) + 
                            ' can not be assigned to type ' + parserUtils.getReversedTypeEnum(type.type));
     }
-        
+    
     symbolTable.setType(declarator.value, type);
+    
+}
+
+/* 
+*   Initializes a struct in the symbol table. 
+*   All members of struct have undefined value.
+*   The value property of a struct variable is a hashmap of tuples, 
+*   with value and type.
+*/
+initializeStruct = function(structDeclarator, structType){
+    var structMembersInitialized = {};
+
+    // Get struct members from original struct specification
+    var structMembersPrototype =  symbolTable.getObject(structType.value);
+    
+    for(var i = 0; i < structMembersPrototype.length; i++){
+        var structProtoMember = structMembersPrototype[i];
+
+        // TODO Consider struct case
+        // TODO Consider pointer case
+        var initializedMember = parserUtils.generateTuple(undefined, structProtoMember.type);
+        structMembersInitialized[structProtoMember.value] = initializedMember;
+    }
+
+    return parserUtils.generateTuple(structMembersInitialized, parserUtils.typeEnum.STRUCT_TYPE);
 }
